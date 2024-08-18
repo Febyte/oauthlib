33115a2d77be7194177e0110670bbe3eae25f920
James Shuriff <james@febyte.com>
2024-08-18 06:35:22 -0400

    added #requires to PowerShell

--- PowerShell/OAuthLib.psm1
+++ PowerShell/OAuthLib.psm1
@@ -1,15 +1,17 @@
+#requires -PSEdition Core
+
 function ConvertTo-Base64Url
 {
 	[CmdletBinding()]
 	Param($InputObject, [switch]$AsPlainText)
-	
+
 	if ($AsPlainText)
 	{
 		$InputObject = [System.Text.Encoding]::UTF8.GetBytes($InputObject)
 	}
-	
+
 	$Base64String = [Convert]::ToBase64String($InputObject)
-	
+
 	$Base64String = $Base64String.Split('=')[0]
 	$Base64String.Replace('+', '-').Replace('/', '_')
 }
@@ -20,21 +22,21 @@ function New-ClientAssertion
 {
 	[CmdletBinding()]
 	Param($ClientId, $TokenEndpointUri, $Pem, [JwtAlgorithm] $Algorithm = 'RS256')
-	
+
 	Begin
 	{
 		$HeaderText = @{alg = $Algorithm.ToString(); typ = 'JWT'} | ConvertTo-Json -Compress
 		$HeaderEncoded = ConvertTo-Base64Url -InputObject $HeaderText -AsPlainText
 	}
-	
+
 	Process
 	{
 		$Now = [DateTime]::UtcNow
 		$Epoch = [DateTime]::new(1970, 01, 01)
-		
+
 		$NotBefore = ($Now.AddMinutes(-5) - $Epoch).TotalSeconds -as [long]
 		$ExpiresAt = ($Now.AddMinutes(5) - $Epoch).TotalSeconds -as [long]
-		
+
 		$Payload = @{
 			sub = $ClientId
 			jti = [Guid]::NewGuid()
@@ -43,13 +45,13 @@ function New-ClientAssertion
 			iss = $ClientId
 			aud = $TokenEndpointUri
 		}
-		
+
 		$PayloadText = $Payload | ConvertTo-Json -Compress
 		$PayloadEncoded = ConvertTo-Base64Url -InputObject $PayloadText -AsPlainText
-		
+
 		$SignaturePayloadText = "${HeaderEncoded}.${PayloadEncoded}"
 		$SignaturePayloadBytes = [System.Text.Encoding]::UTF8.GetBytes($SignaturePayloadText)
-		
+
 		$HashAlgorithm = [System.Security.Cryptography.HashAlgorithmName]::new('SHA' + $Algorithm.ToString().Substring(2))
 
 		switch ($Algorithm.ToString().Substring(0, 2))
@@ -70,9 +72,9 @@ function New-ClientAssertion
 		$AsymmetricAlgorithm.ImportFromPem($Pem)
 		$SignatureBytes = $AsymmetricAlgorithm.SignData($SignaturePayloadBytes, $HashAlgorithm, $SignatureFormat)
 		$AsymmetricAlgorithm.Dispose()
-		
+
 		$SignatureEncoded = ConvertTo-Base64Url -InputObject $SignatureBytes
-		
+
 		"${SignaturePayloadText}.${SignatureEncoded}"
 	}
 }
